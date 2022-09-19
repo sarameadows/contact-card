64f12e56c46e42a7deb7a88fe49ff6a57150dceb
Sara <sameads9@gmail.com>
2022-09-19 09:10:32 -0500

    webpack

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client/'));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.static('../client/dist/'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const htmlRoutes = require('./routes/htmlRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
 
 app.listen(PORT, function() {
     console.log(`Now listening on port: ${PORT}`);
